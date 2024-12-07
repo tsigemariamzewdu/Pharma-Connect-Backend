91b420fa7d89fdd3c251235d2c770200609b9749
NebiyouChanie <cnebiyu@gmail.com>
2024-12-07 20:34:14 +0300

    Connect to mongo DB

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
-
+const connectDB = require('./config/db')
 
 const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+//DB Connect
+connectDB()
+
 
 
 // Testing
 app.get('/', (req, res) => {
     res.send("hello world")
 })
+
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server is listesning on port:", process.env.PORT)
